f42e8836e6225492beb79563a79f5cc5e28a8e0e
Dennis Chacko <dennis.x.chacko@gmail.com>
2020-07-10 19:27:02 -0500

    final?

--- scripts/startservice.sh
+++ scripts/startservice.sh
@@ -1,2 +1,2 @@
-java -jar /deployments/target/SpringRestReferenceApplication-0.0.1-SNAPSHOT.jar
+java -jar /deployments/target/SpringRestReferenceApplication-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 

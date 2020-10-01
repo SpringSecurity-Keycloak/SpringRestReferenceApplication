5534ce391e686783b0afa39fc8b11186c685208b
Dennis Chacko <dennis.chacko@amwell.com>
2020-10-01 11:18:01 -0400

    updated OpenAPI specifications

--- src/main/java/com/cloudxpert/rest/api/StudentApi.java
+++ src/main/java/com/cloudxpert/rest/api/StudentApi.java
@@ -27,55 +27,78 @@ import io.swagger.annotations.Authorization;
 import io.swagger.annotations.AuthorizationScope;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-09-20T11:56:12.166Z[GMT]")
-@Api(value = "v1", description = "The StudentService API")
+@Api(value = "v1", description = "The StudentResourceResourceResourceResourceService API")
 
 public interface StudentApi {
 
-	@ApiOperation(value = "Delete a student", nickname = "deleteV1StudentStudentId", notes = "Delete a student", 
-			authorizations = {
-					@Authorization(value = "OAuth2", 
-							scopes = {
-							@AuthorizationScope(scope = "StudentService-Client1-Scope", description = "Client1&#x27;s scope") 
-							}) 
-			}, 
-			tags = {
-					"Student", 
-			})
-	@ApiResponses(value = { 
-			@ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 404, message = "Not Found"),
-			@ApiResponse(code = 500, message = "Internal Server Error") 
-			})
-	@RequestMapping(value = "/v1/student/{studentId}", method = RequestMethod.DELETE)
-	ResponseEntity<Void> deleteStudent(@ApiParam(value = "", required = true) @PathVariable("studentId") Integer studentId);
+    @ApiOperation(value = "Delete a StudentResource", nickname = "deleteV1StudentResourceStudentResourceId", notes = "Delete a StudentResource", authorizations = {
+        @Authorization(value = "OAuth2", scopes = { 
+            @AuthorizationScope(scope = "StudentResourceService-Client1-Scope", description = "Client1&#x27;s scope")
+            })    }, tags={ "StudentResource", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 404, message = "Not Found"),
+        @ApiResponse(code = 500, message = "Internal Server Error") })
+    @RequestMapping(value = "/api/v1/StudentResource/{StudentResourceId}",
+        method = RequestMethod.DELETE)
+    ResponseEntity<Void> deleteStudent(@ApiParam(value = "",required=true) @PathVariable("StudentResourceId") Integer StudentResourceId
+);
 
-	@ApiOperation(value = "Retrieve All Students", nickname = "getV1StudentList", notes = "Retrieve a list of all the students", response = StudentResource.class, responseContainer = "List", authorizations = {
-			@Authorization(value = "OAuth2", scopes = {
-					@AuthorizationScope(scope = "StudentService-Client1-Scope", description = "Client1&#x27;s scope") }) }, tags = {
-							"Student", })
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "OK", response = StudentResource.class, responseContainer = "List"),
-			@ApiResponse(code = 500, message = "Internal Server Error") })
-	@RequestMapping(value = "/v1/student/list", produces = { "application/json" }, method = RequestMethod.GET)
-	ResponseEntity<List<StudentResource>> getStudentList();
 
-	@ApiOperation(value = "", nickname = "postV1Student", notes = "Add a new Student", response = StudentResource.class, authorizations = {
-			@Authorization(value = "OAuth2", scopes = {
-					@AuthorizationScope(scope = "StudentService-Client1-Scope", description = "Client1&#x27;s scope") }) }, tags = {
-							"Student", })
-	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created", response = StudentResource.class),
-			@ApiResponse(code = 400, message = "Bad Request"),
-			@ApiResponse(code = 500, message = "Internal Server Error") })
-	@RequestMapping(value = "/v1/student", produces = { "application/json" }, consumes = {
-			"application/json" }, method = RequestMethod.POST)
-	ResponseEntity<StudentResource> createStudent(@ApiParam(value = "") @Valid @RequestBody StudentResource body);
+    @ApiOperation(value = "Get StudentResource By Id", nickname = "getApiV1StudentResourceStudentResourceId", notes = "Retrieve details for a single StudentResource", response = StudentResource.class, authorizations = {
+        @Authorization(value = "OAuth2", scopes = { 
+            @AuthorizationScope(scope = "StudentResourceService-Client1-Scope", description = "Client1&#x27;s scope")
+            })    }, tags={ "StudentResource", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK", response = StudentResource.class),
+        @ApiResponse(code = 201, message = "Created", response = StudentResource.class) })
+    @RequestMapping(value = "/api/v1/StudentResource/{StudentResourceId}",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    ResponseEntity<StudentResource> retrieveByStudentId(@ApiParam(value = "",required=true) @PathVariable("StudentResourceId") Integer StudentResourceId
+);
 
-	@ApiOperation(value = "Update student details", nickname = "putV1StudentStudentId", notes = "Update a student details", authorizations = {
-			@Authorization(value = "OAuth2", scopes = {
-					@AuthorizationScope(scope = "StudentService-Client1-Scope", description = "Client1&#x27;s scope") }) }, tags = {
-							"Student", })
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 404, message = "Not Found"),
-			@ApiResponse(code = 500, message = "Internal Server Error", response = StudentResource.class) })
-	@RequestMapping(value = "/v1/student/{studentId}", produces = { "application/json" }, consumes = {
-			"application/json" }, method = RequestMethod.PUT)
-	ResponseEntity<Void> updateStudent(@ApiParam(value = "") @Valid @RequestBody StudentResource body);
+
+    @ApiOperation(value = "Retrieve All StudentResources", nickname = "getV1StudentResourceList", notes = "Retrieve a list of all the StudentResources", response = StudentResource.class, responseContainer = "List", authorizations = {
+        @Authorization(value = "OAuth2", scopes = { 
+            @AuthorizationScope(scope = "StudentResourceService-Client1-Scope", description = "Client1&#x27;s scope")
+            })    }, tags={ "StudentResource", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK", response = StudentResource.class, responseContainer = "List"),
+        @ApiResponse(code = 500, message = "Internal Server Error") })
+    @RequestMapping(value = "/api/v1/StudentResource/list",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    ResponseEntity<List<StudentResource>> getStudentList();
+
+
+    @ApiOperation(value = "Update StudentResource", nickname = "postV1StudentResource", notes = "Add a new StudentResource", response = StudentResource.class, authorizations = {
+        @Authorization(value = "OAuth2", scopes = { 
+            @AuthorizationScope(scope = "StudentResourceService-Client1-Scope", description = "Client1&#x27;s scope")
+            })    }, tags={ "StudentResource", })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 201, message = "Created", response = StudentResource.class),
+        @ApiResponse(code = 400, message = "Bad Request"),
+        @ApiResponse(code = 500, message = "Internal Server Error") })
+    @RequestMapping(value = "/api/v1/StudentResource",
+        produces = { "application/json" }, 
+        consumes = { "application/json" },
+        method = RequestMethod.POST)
+    ResponseEntity<StudentResource> createStudent(@ApiParam(value = ""  )  @Valid @RequestBody StudentResource body
+);
+
+
+    @ApiOperation(value = "", nickname = "putApiV1StudentResource", notes = "Update details for a single StudentResource", authorizations = {
+        @Authorization(value = "OAuth2", scopes = { 
+            @AuthorizationScope(scope = "StudentResourceService-Client1-Scope", description = "Client1&#x27;s scope")
+            })    }, tags={  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "OK") })
+    @RequestMapping(value = "/api/v1/StudentResource",
+        consumes = { "application/json" },
+        method = RequestMethod.PUT)
+    ResponseEntity<Void> updateStudent(@ApiParam(value = ""  )  @Valid @RequestBody StudentResource body
+);
+
+	    
 }
